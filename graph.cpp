bdaabced144e6f643b32ae69ec14632d6aa6bd33
Rajarshi Tiwari <62015785+rajarshi-hub@users.noreply.github.com>
2022-02-11 19:04:17 +0530

    graph tech

---
+++ graph.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    Directed Graph --- 
+    only vis -- will work perfectly fine (If dont want to visit any node again)
+    only rec stack -- will work fine if there is a path kind of thing and no condition on visited
+    both vis and  rec stack -- if path lind of thing and condition on visited(kind of DP)
+    
+    Undirected Graph -- 
+    vis -- wud work fine (Complete check for cycle as well)
+     --- same as undirected ---
+    */
+    
+
+    return 0;
+}
